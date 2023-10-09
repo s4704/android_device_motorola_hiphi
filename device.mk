920728a71f67841cdc2636128199a4c741108931
s4704 <igorkud00@gmail.com>
2023-10-09 14:35:31 +0300

    hiphi: import oss sensor service
    from https://github.com/LineageOS/android_device_motorola_xpeng/tree/lineage-20/sensors

--- device.mk
+++ device.mk
@@ -490,7 +490,11 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
-    libsensorndkbridge
+    libsensorndkbridge \
+    sensors.hiphi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
