d2cc85b4eefdf1527a0c0b0a309010eabfadd146
s4704 <igorkud00@gmail.com>
2023-10-15 07:51:58 +0300

    hiphi: remove CarrierConfig overlay
    This is now generated by extract script in device tree

--- device.mk
+++ device.mk
@@ -50,7 +50,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     FrameworksResCommon_Sys \
-    CarrierConfigResCommon_Sys \
     CellBroadcastReceiverResCommon_Sys \
     SystemUIResCommon_Sys \
     TelecommResCommon_Sys \
